b25950fffdee4849755bde4d48a03fd50eba28de
jeanclaude-desrosiers <jean-claude.desrosiers@jclaude.xyz>
2022-03-15 01:38:21 -0400

    Corriger la valeur minimum, pour la limite inférieure. Clean le script

--- scripts/mesures.py
+++ scripts/mesures.py
@@ -1,78 +1,62 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import statistics
 import csv
 
 """
 Scripts pour obtenir les statistiques boite moustache du fichier jfreechart-stats.csv
 """
 
-m = []
-u = []
-l = []
-d = []
-s = []
-i = []
+# Utilisés pour le calcul de limite inférieure
+MINS = [
+    0,  # NCLOC
+    0,  # DCP
+    1,  # NOCom
+    1  # WMC
+]
+
+medianes = []
+quartiles_superieurs = []
+quartiles_inferieurs = []
+longueurs = []
+limites_inferieures = []
+limites_superieures = []
 
 data = pd.read_csv('../data/jfreechart-stats.csv')
-colonnes = list(data)
-colonnes.remove('class')
-nb_rangees = data.shape[0]
+del data["class"]
+# Nettoyer le nom des colonnes
+data.columns = [colonne.strip() for colonne in data.columns]
 
-for k in colonnes:
-    mediane = data[k].median()
-    m.append(mediane)
+for i, nom_colonne in enumerate(list(data)):
+    medianes.append(data[nom_colonne].median())
 
-    valeurs_min_mediane = []
-    valeurs_max_mediane = []
+    valeurs_min_mediane = data[nom_colonne]\
+        .where(data[nom_colonne] < medianes[i])
+    valeurs_max_mediane = data[nom_colonne]\
+        .where(data[nom_colonne] >= medianes[i])
 
-    for j in range(nb_rangees):
-        donnee = data.loc[j, k]
+    quartiles_inferieurs.append(valeurs_min_mediane.median())
+    quartiles_superieurs.append(valeurs_max_mediane.median())
 
-        if donnee < mediane:
-            valeurs_min_mediane.append(donnee)
-        else:
-            valeurs_max_mediane.append(donnee)
+    longueurs.append(quartiles_superieurs[i] - quartiles_inferieurs[i])
 
-    u.append(statistics.median(valeurs_max_mediane))
-    l.append(statistics.median(valeurs_min_mediane))
-
-for index in range(len(colonnes)):
-    d.append(u[index]-l[index])
-    s.append((u[index] + 1.5*d[index]))
-
-    limite_inferieure = l[index] - 1.5*d[index]
-    if limite_inferieure < 0:
-        limite_inferieure = 0
-    i.append(limite_inferieure)
-
+    limites_inferieures.append(max(
+        MINS[i],
+        quartiles_inferieurs[i] - 1.5 * longueurs[i]
+    ))
+    limites_superieures.append(quartiles_superieurs[i] + 1.5 * longueurs[i])
 
 with open('../data/statistiques.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["Statistiques", "NCLOC", "DCP", "NOCom", "WMC"])
-    writer.writerow(["Médiane", m[0], m[1], m[2], m[3]])
-    writer.writerow(["Quartile supérieur", u[0], u[1], u[2], u[3]])
-    writer.writerow(["Quartile inférieur", l[0], l[1], l[2], l[3]])
-    writer.writerow(["Longueur", d[0], d[1], d[2], d[3]])
-    writer.writerow(["Limite supérieure", s[0], s[1], s[2], s[3]])
-    writer.writerow(["Limite inférieure", i[0], i[1], i[2], i[3]])
+    writer.writerow(["Statistiques"] + list(data))
+    writer.writerow(["Médiane"] + medianes)
+    writer.writerow(["Quartile supérieur"] + quartiles_superieurs)
+    writer.writerow(["Quartile inférieur"] + quartiles_inferieurs)
+    writer.writerow(["Longueur"] + longueurs)
+    writer.writerow(["Limite supérieure"] + limites_superieures)
+    writer.writerow(["Limite inférieure"] + limites_inferieures)
 
-boxplot_NCLOC = data.boxplot(showfliers=False, grid=False, column=[' NCLOC'])
-boxplot_NCLOC.plot()
-
-plt.show()
-
-boxplot_DCP = data.boxplot(showfliers=False, grid=False, column=[' DCP'])
-boxplot_DCP.plot()
-
-plt.show()
-
-boxplot_NOCom = data.boxplot(showfliers=False, grid=False, column=[' NOCom'])
-boxplot_NOCom.plot()
-
-plt.show()
-
-boxplot_WMC = data.boxplot(showfliers=False, grid=False, column=['WMC'])
-boxplot_WMC.plot()
-
-plt.show()
+for nom_colonne in list(data):
+    data.boxplot(showfliers=False, grid=False, column=[nom_colonne]).plot()
+    plt.title(
+        f"Diagramme de boîte pour la mesure {nom_colonne},\n pour le projet jfreechart")
+    plt.show()
