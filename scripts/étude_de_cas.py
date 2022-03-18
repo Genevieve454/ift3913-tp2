a0b84673f2be550fb872c8ecba5d131bd878cdf4
jeanclaude-desrosiers <jean-claude.desrosiers@jclaude.xyz>
2022-03-17 22:55:19 -0400

    Appliquer mes commentaires pour la tâche 2

--- scripts/étude_de_cas.py
+++ scripts/étude_de_cas.py
@@ -1,34 +1,34 @@
 import pandas as pd
+from statistics import geometric_mean
 import csv
 
-
 data = pd.read_csv('../data/jfreechart-stats.csv')
+# Nettoyer le nom des colonnes
+data.columns = [colonne.strip() for colonne in data.columns]
 del data['class']
-del data[' NCLOC']
+del data['NCLOC']
 del data['WMC']
 
 print(data.corr(method='pearson'))
 
-dix_modifications_et_plus = data[data[' NOCom'] >= 10]
-moins_de_dix_modifications = data[data[' NOCom'] < 10]
+collections_donnees = {}
+collections_donnees['NOCom >= 10'] = data[data['NOCom'] >= 10]
+collections_donnees['NOCom < 10'] = data[data['NOCom'] < 10]
 
-nouvelles_donnees = {'DCP_dix_modifications_et_plus': dix_modifications_et_plus[' DCP'],
-                     'DCP_moins_de_dix_modifications': moins_de_dix_modifications[' DCP']}
-
-data = pd.DataFrame(nouvelles_donnees)
-medianes = []
+nom_colonnes = []
+moyennes_geometriques = []
 minimums = []
 maximums = []
 
-for i in list(data):
-    medianes.append(data[i].median())
-    minimums.append(data[i].min())
-    maximums.append(data[i].max())
-
+for nom_colonne, donnees in collections_donnees.items():
+    nom_colonnes.append(nom_colonne)
+    moyennes_geometriques.append(round(geometric_mean(donnees['DCP']), 2))
+    minimums.append(donnees['DCP'].min())
+    maximums.append(donnees['DCP'].max())
 
 with open('../data/etude_de_cas.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["Statistiques"] + list(data))
-    writer.writerow(["Médiane"] + medianes)
+    writer.writerow(["Statistiques"] + nom_colonnes)
+    writer.writerow(["Moyennes Geometriques"] + moyennes_geometriques)
     writer.writerow(["Maximums"] + maximums)
     writer.writerow(["Minimums"] + minimums)
